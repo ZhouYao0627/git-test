cad6eca2c7fcac7b143e8057024d966fbfae646c
ZhouYao <1607435943@qq.com>
2021-08-22 10:50:59 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @author：ZhouYao
+ * @create：2021-08-22 10:39
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+
+
+    }
+
+}
